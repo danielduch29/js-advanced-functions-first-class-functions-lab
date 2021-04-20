768ffe20317c5bdcdecfbe99046f77226587f2f0
Daniel <Daniel.duchatellier@gmail.com>
2021-04-20 12:15:21 -0400

    commit tree e4f72297bcae375438e7dd98e133c1d03c40796f

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+
+function returnFirstTwoDrivers(){
+    const copyOfDrivers = drivers.slice(0,2);
+    return copyOfDrivers
+}
+
+
+function returnLastTwoDrivers(){
+    const copyOfDrivers = drivers.slice(2,4);
+    return copyOfDrivers;
+}
+
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+//so whenver passing functions into an array do not use function, (), {}
+
+
+function createFareMultiplier(num){
+    return function(){return num ** 2}
+}
+
+
+const fn = function(){return createFareMultiplier()}
+
+function fareDoubler(fn){
+    return fn * 2;
+}
+
+function fareTripler(fn){
+    return fn * 3;
+}
+
+function selectDifferentDrivers(drivers, someFn){
+    return someFn(drivers);
+}
